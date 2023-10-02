d1c40aca66538086bceec0bd709410e5886bb3b3
Bruno Pierri <pierri95@gmail.com>
2023-10-02 17:51:44 -0300

    insert news routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AppComponent } from './app.component';
+import { ProjectsComponent } from './pages/projects/projects.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
 
 const routes: Routes = [
-  {path: 'about', component: AboutComponent}
+  {path: '', redirectTo: '/home', pathMatch: 'full'},
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'projects', component: ProjectsComponent},
+  {path: 'contacts', component: ContactsComponent},
 ];
 
 @NgModule({
